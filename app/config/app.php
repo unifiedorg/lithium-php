fa9f9e5fee013ca4d664fc06cef549ff16b5cfdb
Unium <81354905+TheUnium@users.noreply.github.com>
2023-05-24 14:24:40 +0530

    update(app.php): Updated app.php config file to define APP_ICON
    Signed-off-by: Unium <81354905+TheUnium@users.noreply.github.com>

--- app/config/app.php
+++ app/config/app.php
@@ -4,3 +4,4 @@ if (!defined('APP_VERSION')){                    define('APP_VERSION', 'v1.0.0')
 if (!defined('LITHIUM_VERSION')){                define('LITHIUM_VERSION', 'v1.0.0');}
 if (!defined('APP_AUTHOR')){                     define('APP_AUTHOR', 'Unified');}
 if (!defined('APP_DESCRIPTION')){                define('APP_DESCRIPTION', 'A basic app using Lithium.');}
+if (!defined('APP_ICON')){                       define('APP_ICON', '#');}
